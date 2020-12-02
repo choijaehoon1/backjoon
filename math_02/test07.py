a6f62afd79c768af8c8369fc9caf402ef722bb8d
최재훈 <wogns_20@naver.com>
2020-12-02 16:30:38 +0900

    test07.py commit

--- math_02/test07.py
+++ math_02/test07.py
@@ -1,9 +1,16 @@
-while True:
-    array = list(map(int, input().split()))
-    array.sort()
-    if array[0] == 0 and array[1] == 0 and array[2] ==0:
-        break
-    if pow(array[0],2) + pow(array[1],2) == pow(array[2],2):
-        print("right")
-    else:
-        print("wrong")    
+array_x = []
+array_y = []
+for _ in range(3):
+    x,y = map(int, input().split())
+    array_x.append(x)
+    array_y.append(y)
+
+answer = [0,0]
+for i in range(len(array_x)):
+    cnt_x = array_x.count(array_x[i])
+    cnt_y = array_y.count(array_y[i])
+    if cnt_x == 1:
+        answer[0] = array_x[i]
+    if cnt_y == 1:
+        answer[1] = array_y[i]
+print(*answer)        
